cd33a515413b89cacced6ab5c241fcf05832e557
eunjung04 <peunj04@gmail.com>
2020-04-20 19:53:36 +0900

    간단한 이벤트 처리 연습

--- app/src/androidTest/java/fragements/MainFragement.kt
+++ app/src/androidTest/java/fragements/MainFragement.kt
@@ -1,8 +1,26 @@
 package fragements
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.test.espresso.action.AdapterViewProtocol
 import com.example.fragementmore.BasicActivity
+import com.example.fragementmore.R
+import java.util.zip.Inflater
 
 class MainFragement : Fragement() {
 
     override  fun onCreatView(
+        inflater: LayoutInflater,
+        container : ViewGroup?,
+        savedInstanceState : Bundle?
+
+    ): View? {
+
+        return inflater.inflate(R.layout.fragement,container,false)
+    }
 }
+
+f
